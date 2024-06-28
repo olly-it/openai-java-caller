52eb99e1aa36d383053e5a191c7cc3489c201c92
Alessio Olivieri <alessio.olivieri@enginius.com>
2024-06-28 09:52:34 +0200

    chat model test

--- src/test/java/it/olly/openai/OpenaiJavaCallerApplicationTests.java
+++ src/test/java/it/olly/openai/OpenaiJavaCallerApplicationTests.java
@@ -3,9 +3,13 @@ package it.olly.openai;
 import java.util.List;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.ai.chat.model.ChatModel;
+import org.springframework.ai.chat.model.ChatResponse;
+import org.springframework.ai.chat.prompt.Prompt;
 import org.springframework.ai.embedding.EmbeddingModel;
 import org.springframework.ai.embedding.EmbeddingRequest;
 import org.springframework.ai.embedding.EmbeddingResponse;
+import org.springframework.ai.openai.OpenAiChatOptions;
 import org.springframework.ai.openai.OpenAiEmbeddingOptions;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -15,8 +19,14 @@ class OpenaiJavaCallerApplicationTests {
 	
 	@Autowired
 	EmbeddingModel embeddingModel;
-
 	
+	@Autowired
+	ChatModel chatModel;
+
+
+	/**
+	 * https://docs.spring.io/spring-ai/reference/api/embeddings/openai-embeddings.html
+	 */
 	@Test
 	void simpleTestEmbeddings() {
 		System.out.println("DOING simpleTestEmbeddings...");
@@ -32,6 +42,25 @@ class OpenaiJavaCallerApplicationTests {
 			System.out.println(texts.get(i)+" -> "+embeddings.get(i));
 		}
 		System.out.println("simpleTestEmbeddings DONE");
+	}
+	
+	/**
+	 * https://docs.spring.io/spring-ai/reference/api/chat/openai-chat.html
+	 */
+	@Test
+	void simpleTestChat() {
+		System.out.println("DOING simpleTestChat...");
+		/*ChatResponse chatResponse = chatModel.call(
+			    new Prompt(
+			        "Generate the names of 5 famous pirates.",
+			        OpenAiChatOptions.builder()
+			            .withModel("gpt-4-o")
+			            .withTemperature(0.4f)
+			        .build()));*/
+		String question = "Generate the names of 5 famous pirates.";
+		String response = chatModel.call(question);
+		System.out.println(question+" -> "+response);
+		System.out.println("simpleTestChat DONE");
 	}
 
 }
